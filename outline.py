68e5da5114b4c084ef4d5399f8f200593e94fcee
Jacob <logicmaster5823@gmail.com>
2024-10-25 20:06:53 +0100

    Draws an outline
    Only really works with images that are black and white, but hey, fuck around and find out

--- outline.py
+++ outline.py
@@ -9,7 +9,7 @@ TOPLEFT = (665, 334)
 BOTTOMRIGHT = (1574, 842)
 WIDTH = 909
 HEIGHT = 508
-QUALITY = 8
+QUALITY = 3
 
 colors = [\
     SelectableColor(0, 0, 0, 517, 423, 1),\
@@ -32,11 +32,18 @@ colors = [\
     SelectableColor(254, 175, 168, 608, 661, 1),\
 ]
 
+mouse = Controller()
+
+def Click():
+    mouse.click(Button.left)
+
+def Move(position):
+    mouse.position = position
+
 print("Input the file")
 filename = input()
 img = Image.open(filename)
 rgb_img = img.convert('RGB')
-mouse = Controller()
 
 imageRatio = img.width / img.height
 frameRatio = WIDTH / HEIGHT
@@ -48,31 +55,14 @@ yScale = img.height / HEIGHT
 
 scale = xScale if xScale > yScale else yScale
 
-drawWidth = (img.width / scale) / QUALITY
-drawHeight = (img.height / scale) / QUALITY
-
-if imageRatio == frameRatio:
-    scale = WIDTH / img.width
-    drawWidth = WIDTH
-    drawHeight = HEIGHT
-    startPosition = (665, 334)
-elif imageRatio > frameRatio:
-    scale = WIDTH / img.width
-    drawWidth = WIDTH
-    drawHeight = round(img.height * scale)
-    startPosition = (665, round(588 - drawHeight / 2))
-else:
-    scale = HEIGHT / img.height
-    drawWidth = round(img.width * scale)
-    drawHeight = HEIGHT
-    startPosition = (round(1119.5 - drawWidth / 2), 334)
+drawWidth = math.floor((img.width / scale) / QUALITY)
+drawHeight = math.floor((img.height / scale) / QUALITY)
+startPosition = (math.floor(1119.5 - (drawWidth * QUALITY) / 2), math.floor(588 - (drawHeight * QUALITY) / 2))
+Move(startPosition)
 
 def GetPixel(x, y):
     r, g, b = rgb_img.getpixel((x, y))
     return (r, g, b)
-
-#drawWidth = math.floor(drawWidth / QUALITY)
-#drawHeight = math.floor(drawHeight / QUALITY)
 
 ### GENERATE BLACK AND WHITE GRID
 
@@ -80,29 +70,31 @@ print("Generating black and white grid")
 
 black_and_white_grid = []
 
-for x in range(0, (img.width // QUALITY) * QUALITY, QUALITY):
+for x in range(drawWidth):
     column = []
-    for y in range(0, (img.height // QUALITY) * QUALITY, QUALITY):
+    print(f"{round((x) / (drawWidth) * 100, 2)}% at x position of {math.floor(x * QUALITY * scale)} out of {img.width}", end = "\r")
+    for y in range(drawHeight):
         sum = 0
-        for tx in range(QUALITY):
-            for ty in range(QUALITY):
-                pixel = GetPixel((x + tx) / img.width * drawWidth, (y + ty) / img.height * drawHeight)
+        for tx in range(math.floor(QUALITY * scale)):
+            for ty in range(math.floor(QUALITY * scale)):
+                pixel = GetPixel(math.floor(x * QUALITY * scale + tx), math.floor(y * QUALITY * scale + ty))
                 sum += pixel[0] + pixel[1] + pixel[2]
-        if sum / ((QUALITY ** 2) * 3) < 127.5:
-            column.append(False)
-        else:
-            column.append(True)
+        column.append(False if sum / ((QUALITY ** 2) * 3) < 127.5 else True)
     black_and_white_grid.append(column)
 
 ### GENERATE OUTLINES
 
-
-print("Getting outlines")
+print(f"Completed! Generated grid of {drawWidth} width and {drawHeight} height                               ")
+print("Getting outlines and visited grid")
 outline = []
+visited = []
 
 for x in range(drawWidth):
+    print(f"{round((x) / (drawWidth) * 100, 2)}% completed", end = "\r")
     column = []
+    vColumn = []
     for y in range(drawHeight):
+        vColumn.append(False)
         if not black_and_white_grid[x][y]:
             column.append(False)
             continue
@@ -124,20 +116,16 @@ for x in range(drawWidth):
                 continue
         column.append(False)
     outline.append(column)
+    visited.append(vColumn)
 
+print("Completed! Outlines have been created")
 print("Ready to draw! Press enter to begin")
 input()
 
 ### DRAW
-
-def Click():
-    mouse.click(Button.left)
-
-def Move(position):
-    mouse.position = position
     
 def FormatPosition(startPosition, x, y):
-    return (startPosition[0] + x, startPosition[1] + y)
+    return (startPosition[0] + x * QUALITY, startPosition[1] + y * QUALITY)
 
 def SelectColor(x, y):
     if not outline[x][y]:
@@ -145,18 +133,77 @@ def SelectColor(x, y):
     else:
         IndexInfo.selectColorIndex = 0
 
+def SelectDirection(x, y):
+    xInUpperBound = x < drawWidth - 1
+    xInLowerBound = x > 0
+    yInUpperBound = y < drawHeight - 1
+    yInLowerBound = y > 0
+
+    if xInUpperBound:
+        if visited[x + 1][y] == False and outline[x + 1][y] == True:
+            return (x + 1, y), 0
+    if yInUpperBound:
+        if visited[x][y + 1] == False and outline[x][y + 1] == True:
+            return (x, y + 1), 1
+    if xInLowerBound:
+        if visited[x - 1][y] == False and outline[x - 1][y] == True:
+            return (x - 1, y), 2
+    if yInLowerBound:
+        if visited[x][y - 1] == False and outline[x][y - 1] == True:
+            return (x, y - 1), 3
+    if xInUpperBound and yInUpperBound:
+        if visited[x + 1][y + 1] == False and outline[x + 1][y + 1] == True:
+            return (x + 1, y + 1), 4
+    if xInLowerBound and yInUpperBound:
+        if visited[x - 1][y + 1] == False and outline[x - 1][y + 1] == True:
+            return (x - 1, y + 1), 5
+    if xInLowerBound and yInLowerBound:
+        if visited[x - 1][y - 1] == False and outline[x - 1][y - 1] == True:
+            return (x - 1, y - 1), 6
+    if xInUpperBound and yInLowerBound:
+        if visited[x + 1][y - 1] == False and outline[x + 1][y - 1] == True:
+            return (x + 1, y - 1), 7
+    return (-1, -1), -1
+
+def DrawSegment(x, y):
+    cx = x
+    cy = y
+    previousDirection = -2
+    Move(FormatPosition(startPosition, cx, cy))
+    mouse.press(Button.left)
+    while True:
+        newPos, direction = SelectDirection(cx, cy)
+
+        if direction != previousDirection:            
+            Move(FormatPosition(startPosition, cx, cy))
+            time.sleep(0.02)
+
+        cx = newPos[0]
+        cy = newPos[1]
+
+        previousDirection = direction
+
+        if direction == -1:
+            break
+        else:
+            visited[cx][cy] = True
+        
+    mouse.release(Button.left)
+    
+
 def Draw(drawWidth, drawHeight, startPosition):
     for x in range(drawWidth):
         for y in range(drawHeight):
+            if visited[x][y]:
+                continue
+            visited[x][y] = True
             SelectColor(x, y)
-
             if IndexInfo.selectColorIndex == 3:
                 continue
 
-            Move(FormatPosition(startPosition, x, y))
-            Click()
+            DrawSegment(x, y)
+
             if kybrd.is_pressed('#'):
                 return
-            time.sleep(0.1)
 
 Draw(drawWidth, drawHeight, startPosition)
